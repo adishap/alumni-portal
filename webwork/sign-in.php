928582079f5c2aa2ea094de126b4dee026b41f60
MY_EMAIL <porwaladisha@gmail.com>
2014-04-21 21:36:28 +0530

    connecting sign in

--- webwork/sign-in.php
+++ webwork/sign-in.php
@@ -65,9 +65,29 @@
 
       </div>
        <?php
-		$usename = $_POST['username'];
+	     require 'db_connect.php';
+		 
+	   if(isset($_POST['username'])&&isset($_POST['password'])){
+		$username = $_POST['username'];
 		$password = $_POST['password'];
-		
+	   
+	   if(!empty($username)&&!empty($password)){
+		 $query = "SELECT `username` FROM `login` where `username` = '$username' AND `password` = '$password'";
+		 if ($query_run = mysql_query($query)){
+			 $query_num_rows = mysql_num_rows($query_run);
+			 if ($query_num_rows == 0){
+				 echo "Please enter a valid username and password";
+				 }
+			else  
+			{
+				echo "WELCOME!!  " . mysql_result($query_run , 0);
+				}
+			 }
+			 else echo "not working";
+		   }
+	   else 
+	     echo "Please enter username and password.";
+	    }
 		?>
         <div align="center" class="signIn">
     	<form id="form1" name="form1" method="post" action="sign-in.php">
